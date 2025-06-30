9549c1ee1f84441c3ce914311a58f5ff219c2164
leolin0330 <leolin388@gmail.com>
2025-06-30 13:09:18 +0800

    Update check_in_app.py

--- check_in_app.py
+++ check_in_app.py
@@ -5,8 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # 設定 Google Sheets API 權限
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_dict(
-    st.secrets["google_service_account"], scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["google_service_account"], scope)
 client = gspread.authorize(creds)
 
 # 開啟 Google Sheets（請確認名稱與你雲端試算表一致）
